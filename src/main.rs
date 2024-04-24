663cdd3aa3f414a760c558351f917d3fd40d6005
ross_heaney <Rheaney26@gmail.com>
2024-04-24 18:43:35 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-#[allow(unused_imports)]
+use flate2::read::ZlibDecoder;
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
-
+use std::io::Read;
 fn main() {
     //  You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -15,7 +15,24 @@ fn main() {
         fs::create_dir(".git/refs").unwrap();
         fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
         println!("Initialized git directory")
-    } else {
-        println!("unknown command: {}", args[1])
-    }
+    } else if args[1] == "cat-file" {
+        // cat-file -p <blob_sha>
+        if args[2] == "-p" {
+            let sha = &args[3];
+            let content = read_blob_object(sha);
+            println!("{}", content);
+        }
+}
+}
+
+    
+
+// This funcion is used to read a git blob object
+fn read_blob_object(sha: &str) -> String {
+    let path = format!(".git/objects/{}", sha);
+    let compressed = fs::read(path).unwrap();
+    let mut decompressed = ZlibDecoder::new(&compressed[..]);
+    let mut content = Vec::new();
+    decompressed.read_to_end(&mut content).unwrap();
+    String::from_utf8(content).unwrap()
 }

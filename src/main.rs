d6f9ce0c9c08cdee99d8e80f744687bff49cb48e
ross_heaney <Rheaney26@gmail.com>
2024-04-24 21:26:14 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,40 @@
+use clap::Parser;
 use flate2::read::ZlibDecoder;
-use std::env;
-#[allow(unused_imports)]
 use std::fs;
 use std::io::Read;
-fn main() {
-    // println!("Logs from your program will appear here!");
 
-    let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-        println!("Initialized git directory")
-    } else if args[1] == "cat-file" {
-        if args[2] == "-p" {
-            let sha = &args[3];
-            let content = read_blob_object(sha);
-            print!("{}", content);
+#[derive(Parser)]
+struct Opt {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Parser)]
+enum Command {
+    Init,
+    CatFile {
+        #[clap(short, long)]
+        pretty: bool,
+        object: String,
+    },
+}
+
+fn main() {
+    let opt = Opt::parse();
+
+    match opt.command {
+        Command::Init => {
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+            println!("Initialized git directory");
+        }
+        Command::CatFile { pretty, object } => {
+            if pretty {
+                let content = read_blob_object(&object);
+                print!("{}", content);
+            }
         }
     }
 }

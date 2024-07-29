7cb2c67c456b6f731da595e61d4c0e4be480b95c
ross_heaney <Rheaney26@gmail.com>
2024-07-29 19:23:20 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ use std::time;
 
 // GitObject trait defines common methods for all Git objects
 pub trait GitObject {
-    fn serialize(&self) -> Vec<u8>;
-    fn deserialize(&mut self, data: &[u8]);
+    fn compress(&self) -> Vec<u8>;
+    fn decompress(&mut self, data: &[u8]);
     fn fmt(&self) -> &[u8];
 }
 
@@ -25,11 +25,11 @@ impl GitObject for GitBlob {
         b"blob"
     }
 
-    fn serialize(&self) -> Vec<u8> {
+    fn compress(&self) -> Vec<u8> {
         self.blob_data.clone()
     }
 
-    fn deserialize(&mut self, data: &[u8]) {
+    fn decompress(&mut self, data: &[u8]) {
         self.blob_data = data.to_vec();
     }
 }
@@ -50,7 +50,7 @@ pub struct GitTreeLeaf {
 }
 
 // Functions for parsing and handling Git tree objects
-fn tree_parse_one(raw_bytes: &[u8], start_index: usize) -> (GitTreeLeaf, usize) {
+fn parse_git_treee(raw_bytes: &[u8], start_index: usize) -> (GitTreeLeaf, usize) {
     let mut index = start_index;
     let mode = parse_mode(raw_bytes, &mut index);
     let path = parse_path(raw_bytes, &mut index);
@@ -92,18 +92,18 @@ fn parse_sha_hash(raw_bytes: &[u8], index: &mut usize) -> String {
     sha_hash
 }
 
-fn tree_parse(raw_bytes: &[u8]) -> Vec<GitTreeLeaf> {
+fn git_tree_parse(raw_bytes: &[u8]) -> Vec<GitTreeLeaf> {
     let mut index = 0;
     let mut result = Vec::new();
     while index < raw_bytes.len() {
-        let (leaf, new_index) = tree_parse_one(raw_bytes, index);
+        let (leaf, new_index) = parse_git_treee(raw_bytes, index);
         result.push(leaf);
         index = new_index;
     }
     result
 }
 
-fn tree_leaf_sort_key(leaf: &GitTreeLeaf) -> String {
+fn sort_git_tree_keys(leaf: &GitTreeLeaf) -> String {
     if leaf.mode.starts_with(b"10") {
         leaf.path.clone()
     } else {
@@ -121,10 +121,10 @@ impl GitObject for GitTree {
         b"tree"
     }
 
-    fn serialize(&self) -> Vec<u8> {
+    fn compress(&self) -> Vec<u8> {
         let sorted_leaves = {
             let mut leaves = self.leaves.clone();
-            leaves.sort_by_key(tree_leaf_sort_key);
+            leaves.sort_by_key(sort_git_tree_keys);
             leaves
         };
 
@@ -141,8 +141,8 @@ impl GitObject for GitTree {
         result
     }
 
-    fn deserialize(&mut self, data: &[u8]) {
-        self.leaves = tree_parse(data);
+    fn decompress(&mut self, data: &[u8]) {
+        self.leaves = git_tree_parse(data);
     }
 }
 
@@ -156,11 +156,11 @@ impl GitObject for GitCommit {
         b"commit"
     }
 
-    fn serialize(&self) -> Vec<u8> {
+    fn compress(&self) -> Vec<u8> {
         self.commit_data.clone().into_bytes()
     }
 
-    fn deserialize(&mut self, data: &[u8]) {
+    fn decompress(&mut self, data: &[u8]) {
         self.commit_data = String::from_utf8(data.to_vec()).unwrap();
     }
 }
@@ -172,7 +172,7 @@ fn main() {
         "cat-file" => cat_file(&args),
         "hash-object" => hash_object(&args),
         "ls-tree" => ls_tree(&args),
-        "write-tree" => write_tree_command(),
+        "write-tree" => write_new_git_tree_command(),
         "commit-tree" => commit_tree(&args),
         _ => println!("Unknown command: {}", args[1]),
     }
@@ -190,10 +190,10 @@ fn init_git_directory() {
 // Display the contents of a Git object
 fn cat_file(args: &[String]) {
     let hash = &args[args.len() - 1];
-    let object = read_object(hash);
+    let object = read_object_from_store(hash);
     match object {
         GitObjectType::Blob(blob) => {
-            std::io::stdout().write_all(&blob.serialize()).unwrap();
+            std::io::stdout().write_all(&blob.compress()).unwrap();
             std::io::stdout().flush().unwrap();
         }
         _ => println!("Unexpected object type for cat-file"),
@@ -205,16 +205,16 @@ fn hash_object(args: &[String]) {
     let file_path = &args[args.len() - 1];
     let data = fs::read(file_path).unwrap();
     let object = GitBlob { blob_data: data };
-    let contents = object.serialize();
+    let contents = object.compress();
     let object_type = object.fmt();
-    let hash = write_object(contents.as_slice(), object_type);
+    let hash = write_object_to_store(contents.as_slice(), object_type);
     println!("{}", hash);
 }
 
 // List the contents of a Git tree object
 fn ls_tree(args: &[String]) {
     let hash = &args[args.len() - 1];
-    let object = read_object(hash);
+    let object = read_object_from_store(hash);
     match object {
         GitObjectType::Tree(tree) => ls_tree_contents(tree),
         _ => println!("Not a tree object"),
@@ -228,8 +228,8 @@ fn ls_tree_contents(tree: GitTree) {
 }
 
 // Write the current directory structure as a Git tree object
-fn write_tree_command() {
-    let tree_hash = write_tree(".");
+fn write_new_git_tree_command() {
+    let tree_hash = write_new_git_tree(".");
     println!("{}", tree_hash);
 }
 
@@ -241,12 +241,12 @@ fn commit_tree(args: &[String]) {
     let message = &args[message_index + 1];
     let commit_tree_index = args.iter().position(|x| x == "commit-tree").unwrap();
     let tree_hash = &args[commit_tree_index + 1];
-    let commit_hash = commit(tree_hash, message, parent_hash);
+    let commit_hash = write_new_git_commit(tree_hash, message, parent_hash);
     println!("{}", commit_hash);
 }
 
 // Read a Git object from the object store
-fn read_object(hash: &str) -> GitObjectType {
+fn read_object_from_store(hash: &str) -> GitObjectType {
     let path = format!(".git/objects/{}/{}", &hash[..2], &hash[2..]);
     let data = fs::read(path).unwrap();
     let mut decoder = ZlibDecoder::new(data.as_slice());
@@ -261,12 +261,12 @@ fn read_object(hash: &str) -> GitObjectType {
     match object_type {
         b"blob" => {
             let mut blob = GitBlob { blob_data: Vec::new() };
-            blob.deserialize(byte_contents);
+            blob.decompress(byte_contents);
             GitObjectType::Blob(blob)
         }
         b"tree" => {
             let mut tree = GitTree { leaves: Vec::new() };
-            tree.deserialize(byte_contents);
+            tree.decompress(byte_contents);
             GitObjectType::Tree(tree)
         }
         _ => panic!("Unknown object type"),
@@ -274,7 +274,7 @@ fn read_object(hash: &str) -> GitObjectType {
 }
 
 // Write a Git object to the object store
-fn write_object(contents: &[u8], object_type: &[u8]) -> String {
+fn write_object_to_store(contents: &[u8], object_type: &[u8]) -> String {
     let mut result = Vec::new();
     result.extend_from_slice(object_type);
     result.push(b' ');
@@ -299,7 +299,7 @@ fn write_object(contents: &[u8], object_type: &[u8]) -> String {
 }
 
 // Write a directory structure as a Git tree object
-fn write_tree(path: &str) -> String {
+fn write_new_git_tree(path: &str) -> String {
     let mut entries: Vec<(Vec<u8>, String, String)> = Vec::new();
 
     for entry in fs::read_dir(path).unwrap() {
@@ -314,13 +314,13 @@ fn write_tree(path: &str) -> String {
         }
 
         if metadata.is_dir() {
-            let tree_sha_hash = write_tree(entry_path.to_str().unwrap());
+            let tree_sha_hash = write_new_git_tree(entry_path.to_str().unwrap());
             entries.push((mode, file_name, tree_sha_hash));
         } else {
             let blob_contents = fs::read(entry_path.clone()).unwrap();
             let blob = GitBlob { blob_data: blob_contents };
-            let blob_contents = blob.serialize();
-            let sha_hash = write_object(blob_contents.as_slice(), blob.fmt());
+            let blob_contents = blob.compress();
+            let sha_hash = write_object_to_store(blob_contents.as_slice(), blob.fmt());
             entries.push((mode, file_name, sha_hash));
         }
     }
@@ -336,14 +336,14 @@ fn write_tree(path: &str) -> String {
             .collect(),
     };
 
-    let tree_ser = tree.serialize();
-    write_object(tree_ser.as_slice(), tree.fmt())
+    let tree_ser = tree.compress();
+    write_object_to_store(tree_ser.as_slice(), tree.fmt())
 }
 
 // Create a new commit object
-fn commit(tree_hash: &str, message: &str, parent_hash: &str) -> String {
-    let hardcoded_author_name = "Kevin Guo";
-    let hardcoded_author_email = "kev.guo123@gmail.com";
+fn write_new_git_commit(tree_hash: &str, message: &str, parent_hash: &str) -> String {
+    let hardcoded_author_name = "Ross Heaney ";
+    let hardcoded_author_email = "test@gmail.com";
 
     let timestamp = time::SystemTime::now()
         .duration_since(time::UNIX_EPOCH)
@@ -373,6 +373,6 @@ fn commit(tree_hash: &str, message: &str, parent_hash: &str) -> String {
 
     let commit_contents = commit_lines.join("\n");
     let commit = GitCommit { commit_data: commit_contents };
-    let commit_contents = commit.serialize();
-    write_object(commit_contents.as_slice(), commit.fmt())
+    let commit_contents = commit.compress();
+    write_object_to_store(commit_contents.as_slice(), commit.fmt())
 }
